35525974ae9980876bc04131c00b27f3efcf4e9d
vdantr <92918768+vdantr@users.noreply.github.com>
2022-08-14 00:26:13 +0200

    Add guestbook API route

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9ac7fd68.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
